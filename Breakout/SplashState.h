2b67bbf7e4db11496daa978e4bbfda1186d5bcf3
Mariana <Mariana@DESKTOP-N5OAILN>
2018-04-23 17:57:00 -0700

    namnak

--- Breakout/SplashState.h
+++ Breakout/SplashState.h
@@ -1,23 +1,23 @@
-//#pragma once
-//#include <SFML\Graphics.hpp>
-//#include "state.h"
-//#include "game.h"
-//
-//namespace Breakout {
-//
-//	class SplashState : public State
-//	{
-//	public:
-//		SplashState(GamedRef data);
-//		void Init();
-//
-//		void HandleInput();
-//		void Update(float delta);
-//		void Draw(float delta);
-//
-//	private:
-//		GamedRef Gdata;
-//		sf::Clock Gclock;
-//		sf::Texture Gbackground;
-//	};
-//}
+#pragma once
+#include <SFML\Graphics.hpp>
+#include "state.h"
+#include "game.h"
+
+namespace Breakout {
+
+	class SplashState : public State
+	{
+	public:
+		SplashState(GamedRef data);
+		void Init();
+
+		void HandleInput();
+		void Update(float delta);
+		void Draw(float delta);
+
+	private:
+		GamedRef Gdata;
+		sf::Clock Gclock;
+		sf::Texture Gbackground;
+	};
+}
