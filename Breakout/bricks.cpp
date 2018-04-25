83c54600448889c0674392de8086fa36cc8037bf
conno <conno@LAPTOP-FEPLDN16>
2018-04-25 14:44:37 -0700

    brick colors

--- Breakout/bricks.cpp
+++ Breakout/bricks.cpp
@@ -19,6 +19,27 @@ void bricks::setBrickArr(sf::Vector2f & pos, const sf::Color & c, sf::Vector2f &
 		{
 			this->brickArr[j][i] = bricks(pos, c, size);
 
+			if (j == 0)
+			{
+				brickArr[j][i].setFillColor(sf::Color::Red);
+			}
+			if (j == 1)
+			{
+				brickArr[j][i].setFillColor(sf::Color::Yellow);
+			}
+			if (j == 2)
+			{
+				brickArr[j][i].setFillColor(sf::Color::Green);
+			}
+			if (j == 3)
+			{
+				brickArr[j][i].setFillColor(sf::Color::Blue);
+			}
+			if (j == 4)
+			{
+				brickArr[j][i].setFillColor(sf::Color::Magenta);
+			}
+
 			brickArr[j][i].setOutlineThickness(-.6);
 			brickArr[j][i].setOutlineColor(sf::Color::White);
 			brickArr[j][i].setPosition(*(new sf::Vector2f(i * window.getSize().x*0.05, (window.getSize().y / 8) + j * window.getSize().y*.025)));
